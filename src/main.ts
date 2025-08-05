9345ac9f6bd472a690f81aae47d347ce84c1d663
joshuachiher <Joshuachiher@gmail.com>
2025-08-05 13:41:56 +0700

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,28 @@
+import { config } from 'dotenv';
+
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env';
+config({ path: envFile });
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-  import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  console.log("DATABASE_URL:", process.env.DATABASE_URL);  // cek apakah env sudah terbaca
+
   const app = await NestFactory.create(AppModule);
-app.useGlobalPipes(new ValidationPipe({
-  whitelist: true,
-  forbidNonWhitelisted: true,
-  transform: true,
-}));
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
   app.enableCors({
     origin: 'http://localhost:5173', // URL frontend-mu
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-
-
 
   const config = new DocumentBuilder()
     .setTitle('IAG Camp API')
@@ -27,9 +33,8 @@ app.useGlobalPipes(new ValidationPipe({
     .addTag('users')
     .build();
 
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
-
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
